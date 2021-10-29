32fd69dc8dc88d51a449cafe281cced8ce47f274
István Keszericze <50847472+Motkany1980@users.noreply.github.com>
2021-10-29 21:37:00 +0200

    Skeleton

---
+++ src/webparts/part1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPart1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Part1WebPartStrings' {
+  const strings: IPart1WebPartStrings;
+  export = strings;
+}
