5ca7b20ba30e91fe123e1d6d1a5e0240e7e20952
István Keszericze <50847472+Motkany1980@users.noreply.github.com>
2021-10-29 21:56:48 +0200

    alapok beállítva

--- src/webparts/part1/Part1WebPart.ts
+++ src/webparts/part1/Part1WebPart.ts
@@ -21,11 +21,11 @@ export default class Part1WebPart extends BaseClientSideWebPart<IPart1WebPartPro
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Test Site</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <span class="${ styles.title }">Teszt oldal</span>
+              <p class="${ styles.subTitle }">Tetszőlegesen tudom állítani a webpart kijelző tartalmát.</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
+              <a href="https://github.com/Motkany1980/SPFX" class="${ styles.button }" target="_blank">
+                <span class="${ styles.label }">Github</span>
               </a>
             </div>
           </div>
